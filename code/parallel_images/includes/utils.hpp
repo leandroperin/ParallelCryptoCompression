3946ce4dd24de6f5a01e9dc3c389d4b2a3d83700
Leandro Perin <leandro.perino@gmail.com>
2018-08-06 15:02:33 -0300

    Added parallel version for images

---
+++ code/parallel_images/includes/utils.hpp
@@ -0,0 +1,34 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+/***************************************************************************
+*                             INCLUDED FILES
+***************************************************************************/
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <stdlib.h>
+#include <time.h>
+#include "bitfile.hpp"
+#include "consts.hpp"
+#include <omp.h>
+/***************************************************************************
+*                               PROTOTYPES
+***************************************************************************/
+std::vector<int> open_raw_file(FILE *fp);
+bool is_in(int n, std::vector<int> vnData);
+std::vector<int> generate_limited_data(std::vector<int> vnData);
+std::vector<int> generate_random_key(std::vector<int> vnData);
+int max_in_vector(std::vector<int> vnData);
+std::vector<int> generate_coded_vector(std::vector<int> vnData, std::vector<int> K);
+std::string vec2string(std::vector<int> vec);
+std::vector<int> string2vec(std::string sMsg);
+std::vector<int> decode_vector(std::vector<int> cdata, std::vector<int> nLimited, std::vector<int> K);
+std::string vec2compactstring(std::vector<int> vec);
+std::vector<int> compactstring2vec(std::string szString, int nDigits);
+std::vector<int> decode_vector_binary(std::vector<int> cdata, std::vector<int> nUnique, std::vector<int> K);
+std::vector<std::vector<int> > code_vector(std::vector<int> vdata);
+
+#endif
