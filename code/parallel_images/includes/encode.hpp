63440ae0b652a3f5fd544f12d54fcaa37b7394e1
Leandro Perin <leandro.perino@gmail.com>
2018-08-29 17:39:53 -0300

    Some optimizations

--- code/parallel_images/includes/encode.hpp
+++ code/parallel_images/includes/encode.hpp
@@ -9,15 +9,13 @@
 /***************************************************************************
 *                               FUNCTIONS
 ***************************************************************************/
-static void WriteHeader(bit_file_t *bfpOut, stats_t *stats);
-static void ApplySymbolRange(int symbol, stats_t *stats, char model);
-static void SymbolCountToProbabilityRanges(stats_t *stats);
-static int BuildProbabilityRangeList(std::string sMsg, stats_t *stats);
-static void WriteEncodedBits(bit_file_t *bfpOut, stats_t *stats);
 static void WriteRemaining(bit_file_t *bfpOut, stats_t *stats);
+static void WriteEncodedBits(bit_file_t *bfpOut, stats_t *stats);
+static void ApplySymbolRange(int symbol, stats_t *stats, char model);
 static void InitializeAdaptiveProbabilityRangeList(stats_t *stats);
-int ArEncodeString(std::string sMsg, FILE* outFile, const model_t model);
-int myArEncodeString(std::string sMsg, FILE* outFile, const model_t model);
+static void WriteHeader(bit_file_t *bfpOut, stats_t *stats);
+static void SymbolCountToProbabilityRanges(stats_t *stats);
 static int myBuildProbabilityRangeList(std::string sMsg, stats_t *stats);
+int myArEncodeString(std::string sMsg, FILE* outFile, const model_t model);
 
 #endif
